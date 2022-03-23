b9c4f38de74c30445df9f0f0e4d2fdf93fe910e6
kxrt <73589885+kxrt@users.noreply.github.com>
2022-03-23 19:30:18 +0800

    Cleaned up

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 An implementation in Java 11, using a directed cyclic graph.
 
 ## Usage
-* Instantiate an RPS object with a predecided seed.
+* Instantiate an RPS object.
 * Run `play()` with any of the 3 parameters: `ROCK`, `PAPER` or `SCISSORS`.
--- RPS.java
+++ RPS.java
@@ -4,11 +4,11 @@ public class RPS {
   public static GameNode ROCK, PAPER, SCISSORS;
   private final Random r;
 
-  public RPS(int seed) {
+  public RPS( {
     ROCK = new GameNode(SCISSORS);
     PAPER = new GameNode(ROCK);
     SCISSORS = new GameNode(PAPER);
-    r = new Random(seed);
+    r = new Random();
   }
 
   private static class GameNode {
